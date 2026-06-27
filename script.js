e5e301e507513c9892a88e452bbd34182df89f69
cosmiccat9784 <ar101ra@outlook.com>
2026-06-27 12:57:35 +1000

    Switchback Tiles: replace Material Symbols with transparent PNG assets

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function startSwitchbackTiles() {
     `
       <div class="game-layout">
         <div class="game-topline">
-          <span class="game-stat">Goal: connect <span class="material-symbols-outlined icon-start" style="font-size:1rem;vertical-align:middle">play_circle</span> to <span class="material-symbols-outlined icon-end" style="font-size:1rem;vertical-align:middle">flag_2</span></span>
+          <span class="game-stat">Goal: connect <img src="assets/switchback/start.png" class="tile-img-inline" alt="Start" /> to <img src="assets/switchback/end.png" class="tile-img-inline" alt="End" /></span>
           <span class="game-stat" id="switchPuzzle">Puzzle: 1/1</span>
           <span class="game-stat" id="switchMoves">Moves: 0</span>
         </div>
@@ -314,11 +314,11 @@ function startSwitchbackTiles() {
   }
 
   function glyph(tile, index) {
-    if (index === 0) return '<span class="material-symbols-outlined icon-start">play_circle</span>';
-    if (index === 24) return '<span class="material-symbols-outlined icon-end">flag_2</span>';
-    if (tile.type === "line") return '<span class="material-symbols-outlined icon-tile" style="transform:rotate(' + (tile.rotation % 2 ? 0 : 90) + 'deg)">remove</span>';
-    if (tile.type === "corner") return '<span class="material-symbols-outlined icon-tile" style="transform:rotate(' + (tile.rotation * 90) + 'deg)">subdirectory_arrow_right</span>';
-    if (tile.type === "tee") return '<span class="material-symbols-outlined icon-tile" style="transform:rotate(' + (tile.rotation * 90 + 180) + 'deg)">align_flex_end</span>';
+    if (index === 0) return '<img src="assets/switchback/start.png" class="tile-img" alt="Start" />';
+    if (index === 24) return '<img src="assets/switchback/end.png" class="tile-img" alt="End" />';
+    if (tile.type === "line") return '<img src="assets/switchback/straight.png" class="tile-img" style="transform:rotate(' + (tile.rotation * 90) + 'deg)" alt="Pipe" />';
+    if (tile.type === "corner") return '<img src="assets/switchback/turn.png" class="tile-img" style="transform:rotate(' + (tile.rotation * 90) + 'deg)" alt="Pipe" />';
+    if (tile.type === "tee") return '<img src="assets/switchback/junction.png" class="tile-img" style="transform:rotate(' + (tile.rotation * 90) + 'deg)" alt="Pipe" />';
     return ".";
   }
 
