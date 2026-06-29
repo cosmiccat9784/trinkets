e407efb47de3543b8edaf76d7f3f17047a8196b3
cosmiccat9784 <ar101ra@outlook.com>
2026-06-29 16:02:43 +1000

    Fix WASD keys when Caps Lock is on

--- script.js
+++ script.js
@@ -1294,12 +1294,16 @@ function startPocketMaze() {
     const movesByKey = {
       ArrowUp: [0, -1],
       w: [0, -1],
+      W: [0, -1],
       ArrowDown: [0, 1],
       s: [0, 1],
+      S: [0, 1],
       ArrowLeft: [-1, 0],
       a: [-1, 0],
+      A: [-1, 0],
       ArrowRight: [1, 0],
-      d: [1, 0]
+      d: [1, 0],
+      D: [1, 0]
     };
     if (movesByKey[event.key]) {
       event.preventDefault();
